109ccf88a33962ce3bd8920fc589d969a6e32646
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-11-16 14:32:34 -0500

    add: my appointments

--- src/safetech/views/pending-past-appointments/MyAppointments.vue
+++ src/safetech/views/pending-past-appointments/MyAppointments.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <span>adsasdsa</span>
+    <b-row>
+      <b-col cols="6">
+        <b-button
+          class="w-100"
+          :variant="activeTab === 1? 'primary' : 'outline-primary'"
+          :to="{'name': 'my-pending-appointments' }"
+          @click="activeTab = 1"
+        >
+          Proximas citas
+        </b-button>
+      </b-col>
+      <b-col cols="6">
+        <b-button
+          class="w-100"
+          :variant="activeTab === 2? 'primary' : 'outline-primary'"
+          :to="{'name': 'my-old-appointments' }"
+          @click="activeTab = 2"
+        >
+          Historial de citas
+        </b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyAppointments',
+  data() {
+    return {
+      activeTab: 1,
+    }
+  },
 }
 </script>
 
---
+++ src/safetech/views/pending-past-appointments/component/MyAppointmentsCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <pre>brii</pre>
+</template>
+
+<script>
+export default {
+  name: 'MyAppointmentsCards'
+}
+</script>
+
+<style scoped>
+
+</style>
